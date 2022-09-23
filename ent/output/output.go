84ec09034b7006bb77a92bd43bda499cd43255fd
Dmitry Mozzherin <dmozzherin@gmail.com>
2022-09-23 10:36:13 -0500

    add output module

---
+++ ent/output/output.go
@@ -0,0 +1,48 @@
+// package output contains helpers to make results of odds calculation
+// JSON friendly.
+package output
+
+import (
+	"encoding/json"
+	"fmt"
+	"sort"
+
+	"github.com/gnames/bayes/ent/posterior"
+)
+
+type OddsDetails map[string]float64
+
+func New(odds posterior.Odds, cl string) OddsDetails {
+	res := make(OddsDetails)
+	for class, fval := range odds.Likelihoods {
+		if string(class) != cl {
+			continue
+		}
+		for k, v := range fval {
+			str := fmt.Sprintf("%s: %s", k.Name, k.Value)
+			res[str] = v
+		}
+	}
+	return res
+}
+
+type oddsOutput struct {
+	Feature string  `json:"feature"`
+	Odds    float64 `json:"odds"`
+}
+
+func (od OddsDetails) MarshalJSON() ([]byte, error) {
+	odds := make([]oddsOutput, len(od))
+
+	var i int
+	for k, v := range od {
+		odds[i] = oddsOutput{k, v}
+		i++
+	}
+
+	sort.Slice(odds, func(i, j int) bool {
+		return odds[i].Odds > odds[j].Odds
+	})
+
+	return json.Marshal(odds)
+}
